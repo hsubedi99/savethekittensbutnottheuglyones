323b9a0e7f6758436dc4ff5a13906faf7743a983
Hari Subedi <harisubedi99@gmail.com>
2022-06-28 12:30:18 -0400

    Initial Commit

---
+++ SaveTheKitttenExceptTheHairlessOnes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SaveTheKitttenExceptTheHairlessOnes
+//
+//  Created by Hari Subedi on 6/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SaveTheKitttenExceptTheHairlessOnes/SaveTheKitttenExceptTheHairlessOnesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveTheKitttenExceptTheHairlessOnesApp.swift
+//  SaveTheKitttenExceptTheHairlessOnes
+//
+//  Created by Hari Subedi on 6/28/22.
+//
+
+import SwiftUI
+
+@main
+struct SaveTheKitttenExceptTheHairlessOnesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
